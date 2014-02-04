4d414c54dd9872d3a480c4183f9562d3b6648bc1
Brian Lilly <brian@crystalfontz.com>
2014-02-04 15:22:01 -0800

    small changes to hopefully correct all the wireless gui problems with it not starting properly

--- wifi_apps/wifi_connect/wpatest.sh
+++ wifi_apps/wifi_connect/wpatest.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#check if udhcpc is already started for wlan0 if it doesnt, start it
+udhcpAlreadyInstalled=`ps | grep -c -E "udhcpc -R -b -p /var/run/udhcpc.wlan0.pid"`
+if [ $udhcpAlreadyInstalled -eq 1 ]; then
+       udhcpc -R -b -p /var/run/udhcpc.wlan0.pid -i wlan0
+       sleep 1
+fi
+
 #start the supplicant in case it is not running already
 killall wpa_supplicant
 sleep 5
@@ -7,4 +14,4 @@ wpa_supplicant -d -Dwext,rtl8192cu -c/etc/wpa_supplicant.conf -iwlan0 -B
 echo "starting wpa_supplicant"
 sleep 1
 
-$1 -p  /run/wpa_supplicant -i wlan0
+wpa_gui-e -i wlan0 -geometry 480x240+0+0
