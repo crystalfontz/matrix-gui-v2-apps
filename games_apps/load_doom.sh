3adb956284b14ac71e3bd9dc53becfc3a58c15ec
Brian Lilly <brian@crystalfontz.com>
2014-02-04 11:33:05 -0800

    fixed the idiotic recursion when loading doom, started the wpatest.sh differently

--- games_apps/load_doom.sh
+++ games_apps/load_doom.sh
@@ -3,9 +3,9 @@
 pidof matrix_browser > /dev/null 2>&1
 if [ $? == 0 ]
 then
-        load_doom.sh
+        doom.sh
 else
 export TSLIB_TSDEVICE=/dev/input/touchscreen0
 export QWS_MOUSE_PROTO=Tslib:/dev/input/touchscreen0
-        load_doom.sh
+        doom.sh
 fi
